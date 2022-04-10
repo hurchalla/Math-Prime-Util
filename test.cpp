8ed7804c78c2df92d7cc75cb1aebecc0cfd987e9
hurchalla <hurchalla@yahoo.com>
2022-04-09 19:13:23 -0700

    add comparative benchmark test

---
+++ test.cpp
@@ -0,0 +1,109 @@
+
+#include "hurchalla/factoring/factorize.h"
+#include "hurchalla/util/traits/ut_numeric_limits.h"
+#include <inttypes.h>
+#include <iostream>
+#include <chrono>
+#if defined(__GNUC__)
+#  include <cpuid.h>
+#  include <string>
+#  include <cstring>
+#endif
+
+#ifndef NDEBUG
+#error "asserts are enabled and will slow performance"
+#endif
+
+
+typedef unsigned long UV;
+//extern int factor(UV n, UV *factors);
+#define MPU_MAX_FACTORS 64
+
+extern "C" {
+    int factor(UV n, UV *factors);
+};
+
+
+#if defined(__GNUC__)
+std::string displayCPU()
+{
+    // this code is copied from https://stackoverflow.com/a/50021699
+    // licensed CC-BY-SA-3.0  https://creativecommons.org/licenses/by-sa/3.0/
+    char CPUBrandString[0x40];
+    unsigned int CPUInfo[4] = {0,0,0,0};
+
+    __get_cpuid(0x80000000, &CPUInfo[0], &CPUInfo[1], &CPUInfo[2], &CPUInfo[3]);
+    unsigned int nExIds = CPUInfo[0];
+
+    std::memset(CPUBrandString, 0, sizeof(CPUBrandString));
+
+    for (unsigned int i = 0x80000000; i <= nExIds; ++i)
+    {
+        __get_cpuid(i, &CPUInfo[0], &CPUInfo[1], &CPUInfo[2], &CPUInfo[3]);
+
+        if (i == 0x80000002)
+            memcpy(CPUBrandString, CPUInfo, sizeof(CPUInfo));
+        else if (i == 0x80000003)
+            memcpy(CPUBrandString + 16, CPUInfo, sizeof(CPUInfo));
+        else if (i == 0x80000004)
+            memcpy(CPUBrandString + 32, CPUInfo, sizeof(CPUInfo));
+    }
+    std::string result("CPU Type: ");
+    result += CPUBrandString;
+    return result;
+}
+#endif
+
+
+void test64()
+{
+    using namespace hurchalla;
+    using namespace std::chrono;
+    using dsec = duration<double>;
+
+    using T = int64_t;
+    T max = ut_numeric_limits<T>::max();
+    T x;
+    T end = max - 300000;
+
+    std::cout << "\n***Math-Prime-Util***\n";
+    for (int i=0; i<4; ++i) {
+        auto t0 = steady_clock::now();
+        for (x = max; x >= end; x = x-2) {
+            int num_factors;
+            static_assert(sizeof(UV) == sizeof(std::uint64_t), "");
+            UV factors[MPU_MAX_FACTORS+1];
+            num_factors = factor(x, factors);
+            if (factors[0] == 0)
+              break;
+        }
+        if (x >= end)
+          std::cout << "impossible64\n";
+        auto t1 = steady_clock::now();
+        std::cout << dsec(t1-t0).count() << "\n";
+    }
+
+    std::cout << "\n***Factor-Hurchalla***\n";
+    for (int i=0; i<4; ++i) {
+        auto t0 = steady_clock::now();
+        for (x = max; x >= end; x = x-2) {
+            int num_factors;
+            auto factors = factorize(x, num_factors);
+            if (factors[0] == 0)
+              break;
+        }
+        if (x >= end)
+          std::cout << "impossible64\n";
+        auto t1 = steady_clock::now();
+        std::cout << dsec(t1-t0).count() << "\n";
+    }
+}
+
+
+int main()
+{
+#if defined(__GNUC__)
+   std::cout << displayCPU() << "\n";
+#endif
+    test64();
+}
